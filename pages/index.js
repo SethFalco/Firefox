a5d35c032dbd39df39e74a15ace080288e558004
Seth Falco <seth@falco.fun>
2021-11-09 09:50:28 +0100

    inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,52 @@
-import Link from 'next/link'
+import Head from 'next/head'
+
 export default function Home() {
+  const sources = [
+    {
+      src: "/1080.webp",
+      srcSet: "/1080.webp 1x, /2160.webp 2x",
+      media: "(min-width: 769px)"
+    },
+    {
+      src: "/353.webp",
+      srcSet: "/353.webp 1x, /706.webp 2x",
+      media: "(min-width: 481px) and (max-width: 768px)"
+    },
+    {
+      src: "/362.webp",
+      srcSet: "/362.webp 1x. /724.webp 2x",
+      media: "(max-width: 480px)"
+    }
+  ]
+
+  const firstSource = sources[0];
+
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
+      <Head>
+        {sources.map((source) => (
+          <link
+            key={source.src}
+            rel="preload"
+            as="image"
+            href={source.src}
+            imagesrcset={source.srcSet}
+            media={source.media}
+          />
+        ))}
+      </Head>
+
+      <picture key={`image-${firstSource.src}-${firstSource.srcSet}`}>
+        {sources.map((source) => (
+          <source
+            key={source.src}
+            srcSet={source.srcSet}
+            media={source.media}
+          />
+        ))}
+
+        <img src={firstSource.src} srcSet={firstSource.srcSet}/>
+      </picture>
     </div>
   )
 }
